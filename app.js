b1e7db19e1e8ae186f2b51579132340ba1cbaf91
Juan Cruz Cassano <jcassano@ar0c02g44zyml85.fibertel.com.ar>
2021-11-19 15:46:45 -0300

    checkout API integrada - faltan pruebas

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 
+var mercadopago = require("mercadopago");
+mercadopago.configurations.setAccessToken(
+  "APP_USR-7198724548949524-111718-2b297f8c7eb619336bf887c5bb4b33e9-1021172797"
+);
+
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
@@ -17,6 +22,42 @@ app.get("/", function (req, res) {
 
 app.get("/detail", function (req, res) {
   res.render("detail", req.query);
+});
+
+app.get("/payform", function (req, res) {
+  res.render("payform.handlebars", req.query);
+});
+
+app.get("/process_payment", function (req, res) {
+  var payment_data = {
+    transaction_amount: Number(req.body.transactionAmount),
+    token: req.body.token,
+    description: req.body.description,
+    installments: Number(req.body.installments),
+    payment_method_id: req.body.paymentMethodId,
+    issuer_id: req.body.issuer,
+    payer: {
+      email: req.body.email,
+      identification: {
+        type: req.body.docType,
+        number: req.body.docNumber,
+      },
+    },
+    notification_url: "https://engipm4kflp8a.x.pipedream.net",
+  };
+
+  mercadopago.payment
+    .save(payment_data)
+    .then(function (response) {
+      res.status(response.status).json({
+        status: response.body.status,
+        status_detail: response.body.status_detail,
+        id: response.body.id,
+      });
+    })
+    .catch(function (error) {
+      res.status(response.status).send(error);
+    });
 });
 
 app.listen(port);
